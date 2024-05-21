078bf3a6036b36f02473f09c2ab3a5621be1d950
bryan100805 <bryantan100895@gmail.com>
2024-05-21 17:46:17 +0800

    First commit

---
+++ mouth-viseme-tweening/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Avatar from './Components/Avatar';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Avatar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
